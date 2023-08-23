209ed6f4c2afbbce88489ae957a41f44ce5aa8b7
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-08-23 17:47:01 +0300

    fix: fixes for data transmission

--- utilities.py
+++ utilities.py
@@ -97,7 +97,7 @@ def run_experiment(layers_config, pretrain_type, loaders, device, init_type, wit
     # classifier.parameters(), lr=config.finetune_rate, momentum=config.fine_tuning_momentum, weight_decay=1e-6
     # )
     optimizer = optim.Adam(classifier.parameters(), lr=Config.finetune_rate, weight_decay=1e-6)
-    scheduler = StepLR(optimizer, 10, 0.5)
+    scheduler = StepLR(optimizer, 5, 0.5)
     # loaders["train_loader"].dataset.transform = data_config.transform_COMMON
     best_total_acc, losses = train_torch_model(classifier, loaders, optimizer, criterion, scheduler, device)
 
