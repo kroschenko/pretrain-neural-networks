b40fcc2b53a6e394527d306bd2d8752e7e3a143a
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-08-26 15:46:43 +0300

    fix: fix error

--- models.py
+++ models.py
@@ -69,7 +69,7 @@ class RBM(RBMBase):
             self.h = nn.Parameter(0.02 * torch.rand(1, n_hid)-0.01)
         self.a_func = a_func
         self.with_sampling = with_sampling
-        self.delta_weights = torch.zeros(self.W.shape).to(device)
+        self.delta_weights = torch.zeros(self.weights.shape).to(device)
         self.delta_v_thresholds = torch.zeros(self.v.shape).to(device)
         self.delta_h_thresholds = torch.zeros(self.h.shape).to(device)
 
@@ -94,26 +94,26 @@ class CRBM(RBMBase):
         if init_type == InitTypes.Kaiming:
             weights = torch.empty(n_hid_channels, n_vis_channels, kernel_size, kernel_size)
             torch.nn.init.kaiming_normal_(weights, nonlinearity="relu")
-            self.W = nn.Parameter(weights)
+            self.weights = nn.Parameter(weights)
         elif init_type == InitTypes.SimpleNormal:
-            self.W = nn.Parameter(0.01 * torch.randn(n_hid_channels, n_vis_channels, kernel_size, kernel_size))
+            self.weights = nn.Parameter(0.01 * torch.randn(n_hid_channels, n_vis_channels, kernel_size, kernel_size))
             self.v = nn.Parameter(0.01 * torch.randn(1, n_vis_channels, 1, 1))
             self.h = nn.Parameter(0.01 * torch.randn(1, n_hid_channels, 1, 1))
         elif init_type == InitTypes.SimpleUniform:
-            self.W = nn.Parameter(
+            self.weights = nn.Parameter(
                 0.02 * torch.rand(n_hid_channels, n_vis_channels, kernel_size, kernel_size) - 0.01
             )
             self.v = nn.Parameter(0.02 * torch.rand(1, n_vis_channels, 1, 1) - 0.01)
             self.h = nn.Parameter(0.02 * torch.rand(1, n_hid_channels, 1, 1) - 0.01)
         self.a_func = a_func
         self.with_sampling = with_sampling
-        self.delta_weights = torch.zeros(self.W.shape).to(device)
+        self.delta_weights = torch.zeros(self.weights.shape).to(device)
         self.delta_v_thresholds = torch.zeros(self.v.shape).to(device)
         self.delta_h_thresholds = torch.zeros(self.h.shape).to(device)
 
     def visible_to_hidden(self, v):
         weighted_sum = torch.convolution(
-            v, self.W, None, stride=[1, 1],
+            v, self.weights, None, stride=[1, 1],
             padding=[0, 0], dilation=[1, 1], transposed=False,
             output_padding=[0, 0], groups=1
         )
@@ -122,7 +122,7 @@ class CRBM(RBMBase):
 
     def hidden_to_visible(self, h):
         weighted_sum = torch.conv_transpose2d(
-            h, self.W, None, stride=[1, 1], padding=[0, 0], output_padding=[0, 0], groups=1, dilation=[1, 1]
+            h, self.weights, None, stride=[1, 1], padding=[0, 0], output_padding=[0, 0], groups=1, dilation=[1, 1]
         )
         output = self.a_func[0](weighted_sum+self.v)
         return output, weighted_sum
