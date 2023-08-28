293b515c778b54b74bbfe3ef2b354740d3e67a03
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-08-28 16:15:46 +0300

    feat: changes in adam train

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ class Config:
     validate_every_epochs = 1
     validation_decay = 3
     test_batch_size = 128
-    include_pretraining_types = [PretrainingType.RBMClassic]
+    include_pretraining_types = [PretrainingType.REBA]
     DATASETS = [DatasetType.CIFAR10]
 
 
