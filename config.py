f75e00a4378e6d245a24a2e557a52e73122345d9
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-08-31 09:46:56 +0300

    feat: changes in architecture

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ class Config:
     validate_every_epochs = 1
     validation_decay = 3
     test_batch_size = 128
-    include_pretraining_types = [PretrainingType.Without]
+    include_pretraining_types = [PretrainingType.RBMClassic]
     DATASETS = [DatasetType.CIFAR10]
 
 
