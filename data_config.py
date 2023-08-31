e3113c1db8f439e5be6898ef708f3bef4163c8e3
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-08-31 11:47:41 +0300

    feat: changes in architecture

--- data_config.py
+++ data_config.py
@@ -59,8 +59,9 @@ transform_CIFAR_train = transforms.Compose(
     [
         transforms.ToTensor(),
         # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
-        transforms.RandomRotation(15),
         transforms.RandomHorizontalFlip(0.5),
+        transforms.RandomAffine(degrees=0, translate=(5, 5)),
+        transforms.RandomRotation(15),
         #
         # transforms.RandomHorizontalFlip(0.5),
         # transforms.RandomVerticalFlip(0.5),
