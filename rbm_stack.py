3d4499efa9fb7a45f28b940d4067ef412451d368
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-09-23 18:06:03 +0300

    feat: changes in params

--- rbm_stack.py
+++ rbm_stack.py
@@ -48,7 +48,8 @@ class RBMStack:
                             if len(self.layers[layer_index]) == 3:
                                 post_processing_actions = self.layers[layer_index][2]
                                 for action in post_processing_actions:
-                                    if not isinstance(action, torch.nn.Dropout):
+                                    if not isinstance(action, torch.nn.Dropout) and not isinstance(action,
+                                                                                               torch.nn.BatchNorm2d):
                                         inputs = action(inputs)
                         # layer_index = (layer_index + 1) % len(self.rbm_stack)
                     print(loss)
