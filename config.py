57d8222eff67b9f293194e184daf25b944b5e4b5
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-10-22 20:24:30 +0300

    feat: change configs

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ class Config:
     momentum_change_epoch = 5
     pretraining_epochs = 10
     pretraining_rate = 0.0001  # 0.00002 # 0.001   0.00001 - MNIST
-    pretraining_rate_reba = 0.0001  # 0.00002 # 0.001  0.00004 - MNIST
+    # pretraining_rate_reba = 0.0001  # 0.00002 # 0.001  0.00004 - MNIST
 
     finetune_rate = 0.0001
     max_finetuning_epochs = 10
