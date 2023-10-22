d7419f07ca81e8c8fcba1da58842c914091438f3
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-10-22 22:02:54 +0300

    feat: change configs

--- rbm_stack.py
+++ rbm_stack.py
@@ -201,7 +201,7 @@ class RBMStack:
                 val_fail_counter = val_fail_counter + 1 if val_loss > prev_val_loss else 0
                 if val_fail_counter == Config.validation_decay:
                     early_stop = True
-                print("val_loss" + str(val_loss))
+                print("val_loss " + str(val_loss))
             epoch += 1
         return losses
 
