36c0e7875b9088ac7c26d527ddc5e2792c1ffcb6
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-10-22 19:35:56 +0300

    feat: change configs

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ class Config:
     validation_decay = 3
     test_batch_size = 128
     freeze_pretrained_layers = False
-    include_pretraining_types = [PretrainingType.Hybrid]
+    include_pretraining_types = [PretrainingType.Without]
     DATASETS = [DatasetType.MNIST]
 
 
