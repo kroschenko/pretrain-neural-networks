c47911bcd05b2672d8e859c4f70dfeb85083c3d7
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-10-26 00:02:59 +0300

    feat: change configs

--- rbm_stack.py
+++ rbm_stack.py
@@ -229,7 +229,7 @@ class RBMStack:
                 if condition is not None:
                     self.rbm_stack[i].weights = nn.Parameter(self.rbm_stack[i].weights[condition])
                 print(self.rbm_stack[i].weights.shape)
-                condition = torch.abs(self.rbm_stack[i].weights).sum(dim=2).sum(dim=2) != 0
+                condition = torch.abs(self.rbm_stack[i].weights).sum(dim=2).sum(dim=2).sum(dim=1) != 0
                 print(condition.shape)
                 print(condition)
                 removed_columns_indices = torch.where(~condition)[0]
