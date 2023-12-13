aaa50ce4438d234350627ab34d2e0b01524f2aa7
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2023-12-13 22:11:15 +0300

    feat: change configs

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ class Config:
     momentum_beg = 0.5
     momentum_end = 0.9
     momentum_change_epoch = 5
-    pretraining_epochs = 1
+    pretraining_epochs = 10
     pretraining_rate = 0.000125  # 0.00002 # 0.001   0.00001 - MNIST
     pretraining_rate_reba = 0.000125   # 0.00002 # 0.001  0.00004 - MNIST
 
@@ -45,7 +45,7 @@ class Config:
     count_attempts_in_experiment = 1
     init_type = InitTypes.SimpleNormal
     with_sampling = False
-    with_reduction = False
+    with_reduction = True
     with_adaptive_rate = False
     reduction_param = 0.01
     layer_train_type = LayerTrainType.PerLayer
